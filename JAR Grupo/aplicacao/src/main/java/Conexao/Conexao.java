9180a14a235ca510f1f7b6fd2505dfdb53490acd
Thigas29 <thiago.rgomes@sptech.school>
2024-05-30 19:26:46 -0300

    ajuste jar 10.3

--- JAR Grupo/aplicacao/src/main/java/Conexao/Conexao.java
+++ JAR Grupo/aplicacao/src/main/java/Conexao/Conexao.java
@@ -16,7 +16,7 @@ public class Conexao {
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost/sentinel_system");
         dataSource.setUsername("root");
-        dataSource.setPassword("root");
+        dataSource.setPassword("urubu100git add");
 
         conexaoDoBanco = new JdbcTemplate(dataSource);
 
